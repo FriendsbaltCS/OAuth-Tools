a7fbe173294caae5b38575bc9f0164795cd244c4
jahAtFriends <jhammer@friendsbalt.org>
2023-11-30 22:38:24 -0500

    Get and export token in browser

--- helper/redirect/script/post-request.js
+++ helper/redirect/script/post-request.js
@@ -1,11 +1,12 @@
+let tokenFileString;
+
 function generatePostRequest() {
     // Get the form data
     let tokenEndpoint = document.getElementById("token-endpoint").value;
     let clientSecret = document.getElementById("client-secret").value;
     let clientId = document.getElementById("client-id").value;
     let authorizationCode = document.getElementById("auth-code").value;
-    let code = encodeURIComponent(document.getElementById("auth-code").value);
-    let redirectUri = getCookie("redirect_uri");
+    let redirectUri = document.getElementById("redirect-uri").value;
 
     // Generate the cURL request
     let curlCommand =  "curl -X POST " + tokenEndpoint + " \\"
@@ -16,6 +17,83 @@ function generatePostRequest() {
     + "\n -d code=" + authorizationCode;
 
     document.getElementById("curl-command").value = curlCommand;
+
+    // Enable the execute button
+    document.getElementById("execute-post-request").disabled = false;
+    document.getElementById("copy-curl-command").disabled = false;
+}
+
+function copyCurlCommand() {
+    let curlCommand = document.getElementById("curl-command").value;
+    navigator.clipboard.writeText(curlCommand);
+}
+
+function executePostRequest() {
+    let tokenEndpoint = document.getElementById("token-endpoint").value;
+    let clientSecret = document.getElementById("client-secret").value;
+    let clientId = document.getElementById("client-id").value;
+    let authorizationCode = decodeURIComponent(document.getElementById("auth-code").value);
+    let redirectUri = decodeURIComponent(getCookie("redirect_uri"));
+
+    let data = new URLSearchParams({
+        grant_type: "authorization_code",
+        redirect_uri: redirectUri,
+        client_id: clientId,
+        client_secret: clientSecret,
+        code: authorizationCode
+    });
+
+    console.log(data);
+
+    fetch(tokenEndpoint, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: data
+    })
+    .then(response => response.json())
+    .then(responseData => {
+        console.log(responseData);
+        generateTokenFileString(responseData);
+    })
+    .catch(error => {
+        console.error(error);
+    })
+    .finally(() => {
+        document.getElementById("execute-post-request").disabled = true;
+    });
+}
+
+function generateTokenFileString(responseData) {
+    let refreshToken = responseData.refresh_token || ""; // Insert a blank string if refresh_token is not present
+
+    let tokenFile = {
+        token: responseData.access_token,
+        refresh_token: refreshToken,
+        token_uri: document.getElementById("token-endpoint").value,
+        client_id: document.getElementById("client-id").value,
+        client_secret: document.getElementById("client-secret").value,
+        scopes: responseData.scope.split(" "),
+        expiry: new Date(Date.now() + responseData.expires_in * 1000).toISOString(),
+    }
+
+    tokenFileString = JSON.stringify(tokenFile, null, 4);
+
+    document.getElementById("download-token-file").disabled = false;
+}
+
+function downloadTokenFile() {
+    if (tokenFileString) {
+        let blob = new Blob([tokenFileString], {type: "text/plain;charset=utf-8"});
+        let url = URL.createObjectURL(blob);
+        let a = document.createElement('a');
+        a.href = url;
+        a.download = 'token.json';
+        a.click();
+    } else {
+        console.log('No token file to download');
+    }
 }
 
 function populateFromCookies() {
@@ -26,6 +104,7 @@ function populateFromCookies() {
     document.getElementById("token-endpoint").value = token_ep;
     document.getElementById("client-secret").value = client_secret;
     document.getElementById("client-id").value = client_id;
+    document.getElementById("redirect-uri").value = getCookie("redirect_uri");
 }
 
 function getCookie(name) {
@@ -50,3 +129,6 @@ populateFromCookies();
 extractCodeFromURL();
 
 document.getElementById("generate-post-request").addEventListener("click", generatePostRequest);
+document.getElementById("execute-post-request").addEventListener("click", executePostRequest);
+document.getElementById("download-token-file").addEventListener("click", downloadTokenFile);
+document.getElementById("copy-curl-command").addEventListener("click", copyCurlCommand);
--- helper/script/url-generator.js
+++ helper/script/url-generator.js
@@ -7,7 +7,8 @@ function generateAuthURL() {
     let scope = document.getElementById("scope").value;
     let state = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
     let response_type = "code";
-    url = `${auth_ep}?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope}&state=${state}&response_type=${response_type}`;
+    let refreshTokens = document.getElementById("refresh-token").checked ? "&access_type=offline" : "";
+    url = `${auth_ep}?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope}&state=${state}&response_type=${response_type}${refreshTokens}`;
     let displayUrl = url.replace(/&/g, "&\n");
 
     document.getElementById("auth-url").value = displayUrl;
@@ -23,11 +24,43 @@ function generateAuthURL() {
     document.cookie = `state=${state}`;
     document.cookie = `scope=${scope}`;
     document.cookie = `redirect_uri=${redirect_uri}`;
+    document.cookie = `auth_ep=${auth_ep}`;
 }
 
 function visitAuthURL() {
     window.open(url, "_blank");
 }
 
+function loadFromCookiesIfAvailable() {
+    let client_id = getCookie("client_id");
+    let client_secret = getCookie("client_secret");
+    let token_ep = getCookie("token_ep");
+    let state = getCookie("state");
+    let scope = getCookie("scope");
+    let redirect_uri = getCookie("redirect_uri");
+    let auth_ep = getCookie("auth_ep");
+
+    if (client_id && client_secret && token_ep && state && scope && redirect_uri) {
+        document.getElementById("client-id").value = client_id;
+        document.getElementById("client-secret").value = client_secret;
+        document.getElementById("token-endpoint").value = token_ep;
+        document.getElementById("scope").value = scope;
+        document.getElementById("redirect-uri").value = redirect_uri;
+        document.getElementById("auth-endpoint").value = auth_ep;
+    }
+}
+
+function getCookie(name) {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        if (cookie.startsWith(name + '=')) {
+            return cookie.substring(name.length + 1);
+        }
+    }
+    return "";
+}
+
+loadFromCookiesIfAvailable();
 document.getElementById("generate-auth-url").addEventListener("click", generateAuthURL);
 document.getElementById("visit-auth-url").addEventListener("click", visitAuthURL);
