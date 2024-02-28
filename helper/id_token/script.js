1c4ef3c6eb3579df8107245fd07e3dbd422aa515
Joel Hammer <jhammer@friendsbalt.org>
2024-02-28 13:33:21 -0500

    Add copy to clipboard functionality

--- helper/id_token/script.js
+++ helper/id_token/script.js
@@ -2,3 +2,10 @@ function handle_response(response) {
     console.log(response);
     document.getElementById('token-text').innerHTML = response.credential;
 }
+
+function copy_to_clipboard() {
+    var token = document.getElementById('token-text').innerHTML;
+    navigator.clipboard.writeText(token);
+}
+
+document.getElementById('copy-button').addEventListener('click', copy_to_clipboard);
